bb418144d6d65d036e90e19ae0f4211012607aab
yashswarup12 <yashswarup12@gmail.com>
2020-06-04 02:28:50 +0530

    Basic Authentication

---
+++ models/leaders.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var LeaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr:{
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader',LeaderSchema);
+
+module.exports = Leaders;
