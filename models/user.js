d62055c7c06d2042f89a9260e42ad74722148b57
yashswarup12 <yashswarup12@gmail.com>
2020-06-05 16:41:10 +0530

    passport

--- models/user.js
+++ models/user.js
@@ -1,17 +1,8 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-
 	admin: {
 		type: Boolean,
 		default: false
@@ -19,4 +10,5 @@ var User = new Schema({
 
 });
 
+User.plugin(passportLocalMongoose);
 module.exports = mongoose.model('User', User);
